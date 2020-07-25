3de76292830ceb93f1e12f64094ea02e8a0954ca
fgd-haha <fgd.freedom@gmail.com>
2020-07-25 18:45:24 +0800

    feat: https

--- src/components/util/Global.vue
+++ src/components/util/Global.vue
@@ -1,7 +1,7 @@
 <!--全局变量，配置文件-->
 
 <script>
-  const domain='http://www.fanguodong.com/';
+  const domain='https://www.fanguodong.com/';
   // const domain='http://127.0.0.1:8000/';
 
   export default
