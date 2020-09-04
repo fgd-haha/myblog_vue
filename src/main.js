301aa458afca3e02885d99108d9c9ce2643effba
fgd-haha <fgd.freedom@gmail.com>
2020-09-04 21:06:10 +0800

    style: 格式化

--- src/main.js
+++ src/main.js
@@ -3,34 +3,30 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-import ElementUI from 'element-ui';
-import 'element-ui/lib/theme-chalk/index.css';
-import 'vue-cookie/build/vue-cookie.js'
-import VueCookies from 'vue-cookies'
-import VueMarkdown from 'vue-markdown'
-import Icon from 'vue-svg-icon/Icon.vue';
-import 'highlight.js/styles/googlecode.css'
-import hljs from 'highlight.js'
-import 'lodash'
-import global_ from './components/util/Global'
-import qs from 'qs'
 
 
-Vue.config.productionTip = false;
+// elementui组件使用
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
 
 Vue.use(ElementUI);
 
-Vue.use(require('vue-cookie'));
+// cookie处理
+import VueCookies from 'vue-cookies'
+import 'vue-cookie/build/vue-cookie.js'
 
 Vue.use(VueCookies);
+Vue.use(require('vue-cookie'));
 Vue.use(require('vue-cookies'));
 
-Vue.prototype.GLOBAL = global_;
-Vue.prototype.qs = qs;
+// markdown组件使用
+import VueMarkdown from 'vue-markdown'
 
 Vue.component('vue-markdown', VueMarkdown);
 
-Vue.component('icon', Icon);
+// markdown高亮处理
+import 'highlight.js/styles/googlecode.css'
+import hljs from 'highlight.js'
 
 Vue.directive('highlight', function (el, binding) {
     setTimeout(function () {
@@ -41,7 +37,29 @@ Vue.directive('highlight', function (el, binding) {
     }, 1);
 });
 
+// 引用全局变量组件
+import global_ from './components/util/Global'
+
+Vue.prototype.GLOBAL = global_;
+
+// qs是一个增加了一些安全性的查询字符串解析和序列化字符串
+import qs from 'qs'
+
+Vue.prototype.qs = qs;
+
+// 实例解析防抖动
+import 'lodash'
+
 let _ = require('lodash');
+
+// 使用一些elementui没有的svg
+import Icon from 'vue-svg-icon/Icon.vue'
+
+Vue.component('vue-svg-icon', Icon);
+
+// 阻止启动生产消息
+Vue.config.productionTip = false;
+
 
 new Vue({
     el: '#app',
